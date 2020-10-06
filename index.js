88d2c1a3831f7a22150432060597842d037fcb78
kmmccall001 <61025051+kmmccall001@users.noreply.github.com>
2020-10-06 18:02:34 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -19,83 +19,18 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    document.getElementById("petananimal").addEventListener("change", picktheanimal);
 
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-
-    let catmedia = new Media('Kitten-Meow-SoundBible.com-1295572573.mp3',
-        function() { console.log('Media constructed'); });
-    let purr = function() {
-        console.log('Playing...');
-        catmedia.play();
-    }
-    document.getElementById('kitten').addEventListener('click', purr, false);
-
-    
-   
-    let wolfmedia = new Media('wolf-howling-sound-effect.mp3',
-        function() { console.log('Media constructed'); });
-    let howl = function() {
-        console.log('Playing...');
-        wolfmedia.play();
-    }
-    document.getElementById('wolf').addEventListener('click', howl, false);
-    
-    
-    
-    let duckmedia = new Media('Duck-quack.mp3',
-    function() { console.log('Media constructed'); });
-    let quack = function() {
-    console.log('Playing...');
-    duckmedia.play();
-    }
-    document.getElementById('duck').addEventListener('click', quack, false);
-
-
-
-    let ravenmedia = new Media('raven-sounds.mp3',
-        function() { console.log('Media constructed'); });
-    let caw = function() {
-        console.log('Playing...');
-        ravenmedia.play();
-    }
-    document.getElementById('raven').addEventListener('click', caw, false);
-
-    
-    let tigermedia = new Media('tiger-sound.mp3',
-        function() { console.log('Media constructed'); });
-    let roar = function() {
-        console.log('Playing...');
-        tigermedia.play();
-    }
-    document.getElementById('tiger').addEventListener('click', roar, false);
-
-
-
-
-    let dreadwolfmedia = new Media('Solas.Isuspectyouhavequestions.mp3',
-        function() { console.log('Media constructed'); });
-    let solas = function() {
-        console.log('Playing...');
-        dreadwolfmedia.play();
-    }
-    document.getElementById('dreadwolf').addEventListener('click', solas, false);
+function ftoc () {
+    var fare = document.getElementById("fahrenheit").value;
+    var cels = (fare - 32)/ 1.8;
+    document.getElementById("celsius").value = cels;
 }
 
-function picktheanimal(){
-    
-    let animallist = document.getElementsByClassName("animal")
-    for (let i = 0; i < animallist.length; i ++){
-        animallist[i].classList.add("hidden")
-    }
-    document.getElementById(document.getElementById("petananimal").value).classList.remove("hidden");
+function ctof () {
+    var cels = document.getElementById("celsius").value;
+    var fare = (1.8 * cels) + 32;
+    document.getElementById("fahrenheit").value = fare;  
 }
-
 
 
